5d85243eaa1c91f8fa127ec4be0ffdb91de788b8
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-02 16:36:42 -0700

    Add create action and strong params for creation of new bulk discount

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -6,11 +6,20 @@ class  Merchant::BulkDiscountsController < ApplicationController
   end
 
 	def new
+		@merchant = Merchant.find(params[:merchant_id])
 		@discount = BulkDiscount.new
 	end
 
 	def create
-		require 'pry'; binding.pry
+		merchant = Merchant.find(params[:merchant_id])
+		merchant.bulk_discounts.create(bulk_discount_params)
+		redirect_to merchant_bulk_discounts_path
+	end
+
+	private
+
+	def bulk_discount_params
+		params.require(:bulk_discount).permit(:percentage, :quantity_threshold)
 	end
 
 end
