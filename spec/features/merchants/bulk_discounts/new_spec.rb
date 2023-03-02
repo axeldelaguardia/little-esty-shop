d1d002625620afb8c0330e2ab5095373a99a7221
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-02 16:37:27 -0700

    Add tests for creation of bulk discount

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -10,8 +10,30 @@ RSpec.describe 'merchant bulk discount new page', type: :feature do
 			it 'there is a form to add a new bulk discount' do
 				visit new_merchant_bulk_discount_path(merchant_1)
 
-				expect(page).to have_field(:percentage)
-				expect(page).to have_field(:quantity_threshold)
+				expect(page).to have_field('Percentage')
+				expect(page).to have_field('Quantity Threshold')
+			end
+
+			it 'creating a new discount' do
+				visit new_merchant_bulk_discount_path(merchant_1)
+				
+				fill_in 'Percentage', with: '50'
+				fill_in 'Quantity Threshold', with: '100'
+				click_button
+
+				expect(page).to have_current_path(merchant_bulk_discounts_path(merchant_1))
+			end
+
+			it 'should have the new discount in the merchant bulk discounts index page' do
+				visit new_merchant_bulk_discount_path(merchant_1)
+				
+				fill_in 'Percentage', with: '50'
+				fill_in 'Quantity Threshold', with: '100'
+				click_button
+
+				expect(page).to have_content('Bulk Discount #3')
+				expect(page).to have_content('Percentage: 50')
+				expect(page).to have_content('Quantity Threshold: 100')
 			end
 		end
   end
