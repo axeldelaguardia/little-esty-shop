503d84608d1b6b49b4c59897595cdee1a8d1ca75
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-07 09:19:00 -0700

    add validatons and tests to bulk discounts

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,4 +2,6 @@ class BulkDiscount < ApplicationRecord
 	belongs_to :merchant
 	has_many :items, through: :merchant
 	has_many :invoice_items, through: :items
+	validates :percentage, presence: true, inclusion: 0..1
+	validates :quantity_threshold, presence: true
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
   it { should belong_to(:merchant) }
+	it { should validate_presence_of :percentage }
+	it { should validate_inclusion_of(:percentage).in_range(0..1) }
+	it { should validate_presence_of :quantity_threshold }
 end
